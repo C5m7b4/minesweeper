d41a41e75d4174f834c9d7a4a061914fbbc07376
C5m7b4 <mbedingfield@dcrpos.com>
2021-12-17 14:39:43 -0600

    Add opencell logic

--- src/helpers/CellsManipulators.ts
+++ src/helpers/CellsManipulators.ts
@@ -1,4 +1,4 @@
-import { Cell, Coords, Field } from './Field';
+import { Cell, CellState, Coords, Field } from './Field';
 
 export const incrementNeibors = (coords: Coords, field: Field): Field => {
   const items = getNeigborsItems(coords);
@@ -31,3 +31,43 @@ export const getNeigborsItems = ([y, x]: Coords): Record<
 
 export const checkItemInfield = ([y, x]: Coords, { length }: Field): boolean =>
   y >= 0 && x >= 0 && length - y > 0 && length - x > 0;
+
+export const openCell = (
+  coords: Coords,
+  playerField: Field,
+  gameField: Field
+): Field => {
+  const { empty, hidden, bomb } = CellState;
+  const [y, x] = coords;
+  const gameCell = gameField[y][x];
+
+  if (gameCell === bomb) {
+    throw new Error('Game Over');
+  }
+
+  if (gameCell == empty) {
+    playerField[y][x] = gameCell;
+
+    const items = getNeigborsItems(coords);
+
+    for (const coords of Object.values(items)) {
+      if (checkItemInfield(coords, gameField)) {
+        const [y, x] = coords;
+
+        const gameCell = gameField[y][x];
+        const playerCell = playerField[y][x];
+
+        if (gameCell === empty && playerCell === hidden) {
+          playerField = openCell(coords, playerField, gameField);
+        }
+        if (gameCell < bomb) {
+          playerField[y][x] = gameCell;
+        }
+      }
+    }
+  }
+
+  playerField[y][x] = gameCell;
+
+  return playerField;
+};
